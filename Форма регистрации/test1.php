eadb3b0fea594327b46cddc5c52e878c4b7636e2
AnastasiaSinitsyna <116564288+AnastasiaSinitsyna@users.noreply.github.com>
2023-08-24 13:29:22 +0200

    Обновление именований+редакция вида формы регистрации

--- Форма регистрации/test1.php
+++
@@ -1,9 +0,0 @@
-<?php //test1.php
-$username = "Fred Smith";
-echo $username;
-echo "<br>";
-$curren_user = $username;
-echo $curren_user;
-$nameGirls = array("Anna", "Helen", "Berta", "Anastasiia", "Yulia");
-echo "<br>";
-echo $nameGirls[1];
